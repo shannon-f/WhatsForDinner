bb93c462d58f6a6c48e9de6f677c28418008116b
shannon-f <shannon_f@icloud.com>
2020-05-04 16:49:45 -0500

    Initial Commit

---
+++ WhatsForDinner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhatsForDinner
+//
+//  Created by Shannon Fluellen on 5/4/20.
+//  Copyright © 2020 Shannon Fluellen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
