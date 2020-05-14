dc432d9e9299f0bbddff7fba85cbd36a1459c0e2
shannon-f <shannon_f@icloud.com>
2020-05-13 20:46:54 -0500

    create day enum, add to create meal view

---
+++ WhatsForDinner/Day.swift
@@ -0,0 +1,19 @@
+//
+//  Day.swift
+//  WhatsForDinner
+//
+//  Created by Shannon Fluellen on 5/13/20.
+//  Copyright © 2020 Shannon Fluellen. All rights reserved.
+//
+
+import Foundation
+
+enum Day : String, CaseIterable {
+    case Monday = "Monday"
+    case Tuesday = "Tuesday"
+    case Wednesday = "Wednesday"
+    case Thursday = "Thursday"
+    case Friday = "Friday"
+    case Saturday = "Saturday"
+    case Sunday = "Sunday"
+}
--- WhatsForDinner/Views/CreateMealView.swift
+++ WhatsForDinner/Views/CreateMealView.swift
@@ -11,18 +11,28 @@ import SwiftUI
 struct CreateMealView: View {
     @State var entree  = ""
     @State var side1  = ""
+    @State var side2  = ""
+    @State var day : Day?
     
     var body: some View {
+       
         VStack {
+            ForEach(Day.allCases, id: \.self) { day in
+            //            Button(){
+            //                Shape()
+            //            }
+                Text(day.rawValue)
+                    }
             Text("New Meal")
             TextField("Entree", text: $entree)
             TextField("Side 1", text: $side1)
+            TextField("Side 2", text: $side2)
         }
     }
 }
 
 struct CreateMealView_Previews: PreviewProvider {
     static var previews: some View {
-        CreateMealView()
+        CreateMealView(day:.Monday)
     }
 }
