782e2bd6065afe05b72ac5325c0b85544b649a89
shannon-f <shannon_f@icloud.com>
2020-05-15 22:25:33 -0500

    add date picker to CreateMealView, move day selector code to another view for possible use later

--- WhatsForDinner/Views/CreateMealView.swift
+++ WhatsForDinner/Views/CreateMealView.swift
@@ -10,29 +10,41 @@ import SwiftUI
 
 struct CreateMealView: View {
     @State var entree  = ""
+    @State var mealName = ""
     @State var side1  = ""
     @State var side2  = ""
-    @State var day : Day?
+//    @State var day : Day?
+    
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        return formatter
+    }
+
+    @State private var mealDate = Date()
     
     var body: some View {
        
         VStack {
-            ForEach(Day.allCases, id: \.self) { day in
-            //            Button(){
-            //                Shape()
-            //            }
-                Text(day.rawValue)
-                    }
-            Text("New Meal")
-            TextField("Entree", text: $entree)
-            TextField("Side 1", text: $side1)
-            TextField("Side 2", text: $side2)
+            DatePicker(selection: $mealDate, in: Date()..., displayedComponents: .date) {
+                Text("Meal date")
+            }.padding(.top)
+//            Spacer()
+            TextField("Meal Name", text: $mealName).padding()
+//            Divider()
+            TextField("Entree", text: $entree).padding()
+            TextField("Side 1", text: $side1).padding()
+            TextField("Side 2", text: $side2).padding()
+            Button(action:{}) {
+                Text("Save Meal")
+            }
+            Spacer()
         }
     }
 }
 
 struct CreateMealView_Previews: PreviewProvider {
     static var previews: some View {
-        CreateMealView(day:.Monday)
+        CreateMealView()
     }
 }
---
+++ WhatsForDinner/Views/DaySelectorView.swift
@@ -0,0 +1,28 @@
+//
+//  DaySelectorView.swift
+//  WhatsForDinner
+//
+//  Created by Shannon Fluellen on 5/15/20.
+//  Copyright © 2020 Shannon Fluellen. All rights reserved.
+//
+
+import SwiftUI
+
+struct DaySelectorView: View {
+    var body: some View {
+        VStack {
+            ForEach(Day.allCases, id: \.self) { day in
+                Button(action: {}){
+                    Text(day.rawValue).background(Circle().border(Color.blue).frame(width: 100, height: 100)).clipped()
+                }
+                
+            }
+        }
+    }
+}
+
+struct DaySelectorView_Previews: PreviewProvider {
+    static var previews: some View {
+        DaySelectorView()
+    }
+}
